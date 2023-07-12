6f18844da8e419b0a2abb8cd9dc94290d87e8d69
yunna <homer_j@hanmail.net>
2023-07-12 21:33:42 +0900

    feat : Outlet 사용하여 자식 컴포넌트 렌더하기

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Root from './Root';
 import NotFound from './screens/NotFound';
 import ErrorComponent from './components/ErrorComponent';
 import User from './screens/users/User';
+import Followers from './screens/users/Followers';
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
     children: [
       { path: '', element: <Home />, errorElement: <ErrorComponent /> },
       { path: 'about', element: <About /> },
-      { path: 'users/:id', element: <User /> },
+      {
+        path: 'users/:id',
+        element: <User />,
+        children: [{ path: 'followers', element: <Followers /> }],
+      },
     ],
     errorElement: <NotFound />,
   },
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import { users } from '../../db';
 
 const User = () => {
@@ -6,9 +6,15 @@ const User = () => {
   console.log(typeof Number(id));
 
   return (
-    <h1>
-      User name is {users[Number(id) - 1].name} and id Number is {id}{' '}
-    </h1>
+    <>
+      <h1>
+        User name is {users[Number(id) - 1].name} and id Number is {id}{' '}
+      </h1>
+      <div>
+        <Link to={'followers'}>followers</Link>
+        <Outlet />
+      </div>
+    </>
   );
 };
 
